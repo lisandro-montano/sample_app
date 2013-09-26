dae9794f48f6927b236af44062ebdd80543ca0d5
app <app@ip-10-0-0-122.ec2.internal>
2013-09-26 16:54:04 +0000

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Lisandro Montano"
+    email    "lisandro@itivitykids.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
